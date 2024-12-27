13dcb097b1e09bd041ba5da6f6135c8c0feca99f
killian-dv <killian.david@etudiant.univ-reims.fr>
2024-12-27 19:07:39 +0100

    feat: first animation with gsap

--- src/main.js
+++ src/main.js
@@ -1,3 +1,4 @@
+import gsap from "gsap";
 import * as THREE from "three";
 import "./style.css";
 
@@ -21,27 +22,25 @@ const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height);
 camera.position.z = 3;
 scene.add(camera);
 
-// camera.lookAt(mesh.position);
-
 // renderer
 const canvas = document.querySelector("#webgl");
 const renderer = new THREE.WebGLRenderer({ canvas });
 renderer.setSize(sizes.width, sizes.height);
 
-// clock
-const clock = new THREE.Clock();
+gsap.to(mesh.position, {
+  x: 2,
+  duration: 1,
+  delay: 1,
+});
+
+gsap.to(mesh.position, {
+  x: 0,
+  duration: 1,
+  delay: 2,
+});
 
 // animation
 const loop = () => {
-  // time
-  const elapsedTime = clock.getElapsedTime();
-
-  // update
-  camera.position.x = Math.cos(elapsedTime);
-  camera.position.y = Math.sin(elapsedTime);
-
-  camera.lookAt(mesh.position);
-
   // render
   renderer.render(scene, camera);
 
